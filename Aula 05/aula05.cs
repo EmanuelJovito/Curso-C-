274ef4c3877b071e193d796f3b42dbef14a1c206
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-10-15 09:23:56 -0300

    Aula 05

---
+++ Aula 05/aula05.cs
@@ -0,0 +1,10 @@
+using System;
+class Aula05 {
+  static void Main() {
+    int res = (10 + 5) * 10;
+    Console.WriteLine(res);
+
+    bool resBoolean = 10 <= 10;
+    Console.WriteLine(resBoolean);
+  }
+}
