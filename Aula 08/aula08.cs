0f60c4a23c0dc2c55a778bec7ac654fe4feac1ae
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-10-18 13:59:09 -0300

    Aula 08

--- Aula 08/aula08.cs
+++ Aula 08/aula08.cs
@@ -1,51 +1,62 @@
 using System;
 class Aula08 {
   static void Main() {
-    int operação;
+    int operação, finalizar;
     string nome, tipo;
+
+    finalizar = 1;
 
     Console.Write("Digite o seu nome de usuário: ");
     nome = Console.ReadLine();
     
     Console.WriteLine("Bem vindo a calculadora, {0}", nome);
 
-    Console.Write("Digite o tipo da operação que deseja realizar (Primeira letra maiuscula): ");
-    tipo = Console.ReadLine();
 
-    if (tipo != "Soma" & tipo != "Multiplicação" & tipo != "Divisão" & tipo != "Subtração") {
+    while (finalizar == 1) {
+      Console.Write("Digite o tipo da operação que deseja realizar: ");
+      tipo = String.ToLower(Console.ReadLine());
+
+      if (tipo != "soma" & tipo != "multiplicação" & tipo != "divisão" & tipo != "subtração") {
       Console.WriteLine("Tipo de operação invalida");
-    } else {
-      int v1, v2;
 
-      Console.Write("Digite o primeiro valor: ");
-      v1 = Convert.ToInt32(Console.ReadLine());
+      } else {
+        int v1, v2;
 
-      Console.Write("Digite o segundo valor: ");
-      v2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Digite o primeiro valor: ");
+        v1 = Convert.ToInt32(Console.ReadLine());
 
-      if (tipo == "Soma") {
-        operação = v1 + v2;
+        Console.Write("Digite o segundo valor: ");
+        v2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("O resultado é igual a: {0}", operação);
-      }
+        if (tipo == "Soma") {
+          operação = v1 + v2;
 
-      if (tipo == "Multiplicação") {
-        operação = v1 * v2;
+          Console.WriteLine("O resultado é igual a: {0}", operação);
+        }
 
-        Console.Write("O resultado é igual a: {0}", operação);
-      }
+        if (tipo == "Multiplicação") {
+          operação = v1 * v2;
 
-      if (tipo == "Divisão") {
-        operação = v1 / v2;
+          Console.WriteLine("O resultado é igual a: {0}", operação);
+        }
 
-        Console.Write("O resultado é igual a: {0}", operação);
-      }
+        if (tipo == "Divisão") {
+          operação = v1 / v2;
 
-      if (tipo == "Subtração") {
-        operação = v1 - v2;
+          Console.WriteLine("O resultado é igual a: {0}", operação);
+        }
 
-        Console.Write("O resultado é igual a: {0}", operação);
+        if (tipo == "Subtração") {
+          operação = v1 - v2;
+
+          Console.WriteLine("O resultado é igual a: {0}", operação);
+        }
+        
+        Console.Write("DIGITE 0 SE DESEJA ENCERRAR E 1 SE DESEJA CONTINUAR: ");
+        finalizar = Convert.ToInt32(Console.ReadLine());
       }
     }
+
+    
   }
 }
