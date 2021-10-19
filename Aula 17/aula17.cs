fa405ff26b7b1db27eca793181aa8243844f1034
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-10-19 16:14:07 -0300

    Aula 17

---
+++ Aula 17/aula17.cs
@@ -0,0 +1,17 @@
+using System;
+class Aula17 {
+  static void Main() {
+    //int n1, n2, n3, n4, n5;
+    int[] n=new int[5];
+    //int[] num=new int[3] {1, 2, 3};
+    int[] num={1, 2, 5};
+
+    n[0] = 11;
+    n[1] = 22;
+    n[2] = 33;
+    n[3] = 44;
+    n[4] = 55;
+
+    Console.WriteLine(num[0]);
+  }
+}
