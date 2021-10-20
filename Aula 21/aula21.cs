2e7ce6ca17dccbcba20d10da7be725178c46d75b
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-10-20 10:36:11 -0300

    Aula 21

---
+++ Aula 21/aula21.cs
@@ -0,0 +1,18 @@
+using System;
+class Aula21 {
+  static void Main() {
+    string senha = "123";
+    string senhaUser;
+    int tentativas = 0;
+
+    do {
+      Console.Clear();
+      Console.Write("Digite a senha: ");
+      senhaUser = Console.ReadLine();
+      tentativas ++;
+    } while(senha != senhaUser);
+
+    Console.Clear();
+    Console.WriteLine("Senha correta, tentativas: {0}", tentativas);
+  }
+}
