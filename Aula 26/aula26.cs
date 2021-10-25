d1dab5ec68ccae7b1017c414988e5665b5d290af
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-10-25 17:09:40 -0300

    Aula 26

--- Aula 26/aula26.cs
+++ Aula 26/aula26.cs
@@ -3,8 +3,10 @@ class Aula25 {
   static void Main() {
     int divid, divis, quoc, rest;
 
-    divid = 10;
-    divis = 5;
+    Console.Write("Digite o primeiro valor: ");
+    divid = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Digite o segundo valor:");
+    divis = Convert.ToInt32(Console.ReadLine());
     quoc = divide(divid, divis, out rest);
 
     Console.WriteLine("{0}/{1}: quociente={2} e resto={3}", divid, divis, quoc, rest);
