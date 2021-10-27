b3ccc67148d6f31a3555e121f6a6a62696590e92
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-10-27 14:55:37 -0300

    Aula 30

---
+++ Aula 30/aula30.cs
@@ -0,0 +1,29 @@
+using System; 
+public class Jogador {
+  public int energia;
+  public bool vivo;
+  public string nome;
+
+  public Jogador(string n) {
+    energia = 100;
+    vivo = true;
+    nome = n;
+  }
+
+  ~Jogador() {
+    Console.WriteLine("Jogador deletado!");
+  }
+}
+class Aula28 {
+  static void Main() {
+    string nome1;
+
+    Console.WriteLine("Digite o nome do jogador 1: ");
+    nome1 = Console.ReadLine();
+
+    Jogador j1 = new Jogador(nome1);
+    Jogador j2 = new Jogador("Emanue");
+
+    Console.WriteLine("Energia do Jogador 1: {0}", j1.nome);
+  }
+}
