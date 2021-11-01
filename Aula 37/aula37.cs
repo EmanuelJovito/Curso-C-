db64b6a8f0557538077267ed229c9ed66f94c0bd
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-11-01 16:49:33 -0300

    Aula 37

---
+++ Aula 37/aula37.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Base {
+  public Base() {
+    Console.WriteLine("Construtor da classe Base");
+  }
+}
+
+class Derivada1: Base {
+  public Derivada1() {
+    Console.WriteLine("Construtor da classe Base derivada 1");
+  }
+}
+
+class Derivada2: Derivada1 {
+  public Derivada2() {
+    Console.WriteLine("Construtor da classe Derivada 2");
+  }
+}
+
+class Aula37 {
+  static void Main() {
+    Derivada2 derivada = new Derivada2();
+  }
+}
